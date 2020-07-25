88c6d844c4bede6196a59dd217c11082caf0a8f3
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-07-25 00:01:14 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,25 +1,25 @@
-class Note < ApplicationRecord
+class Note < ActiveRecord::Base
   belongs_to :user
-   has_many :viewers
-   has_many :readers, through: :viewers, source: :user
-   
-   before_save :ensure_owner_can_read
-   
-   def visible_to
-    readers.map { |u| u.name }.join(', ')
-   end
-   
-   def visible_to=(new_readers)
-    self.readers = new_readers.split(', ').map do |name|
+  has_many :viewers
+  has_many :readers, through: :viewers, source: :user
+
+  before_save :ensure_owner_can_read
+
+  def visible_to
+    readers.map {|reader| reader.name}.join(', ')
+  end
+
+  def visible_to=(comma_string)
+    self.readers = comma_string.split(',').map do |name|
       User.find_by(name: name.strip)
     end.compact
-   end
-   
-   private
-   
-   def ensure_owner_can_read
-    if user && !readers.include?(user)
-      readers << user
+  end
+
+  private
+
+    def ensure_owner_can_read
+      if self.user && !readers.include?(self.user)
+        readers << self.user
+      end
     end
-   end
 end
