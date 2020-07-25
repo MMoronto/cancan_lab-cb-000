8231a08f16a5857c582f22c298915b5b8206c6e6
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-07-25 00:00:18 +0000

    Automatically backed up by Learn

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Note, type: :model do
     it "takes a comma separated list of names, trims spaces, and makes those users readers" do
       note = Note.new      
       note.visible_to = 'alice,beth'      
-      # assert note.readers == [users(:alice), users(:beth)]
+      assert note.readers == [users(:alice), users(:beth)]
     end
   end
 
